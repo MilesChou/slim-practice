b361518c0a187f23d9ba5d673122b8abbf508c5e
MilesChou <jangconan@gmail.com>
2016-08-11 10:22:25 +0800

    Add entry files

---
+++ bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$container = new Slim\Container();
+$container['settings']['displayErrorDetails'] = true;
+
+$app = new \Slim\App($container);
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+return $app;
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+$app = require __DIR__ . '/../bootstrap.php';
+$app->run();
